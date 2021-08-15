aab2f1642982e1489e6f63376d7e31532145e26c
burhansaleemi <76588697+burhansaleemi@users.noreply.github.com>
2021-08-15 22:18:34 +0500

    Create assignment3.sol

---
+++ assignmentQ3_138761/assignment3.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.0;
+//"SPDX-License-Identifier: UNLICENSED"
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract erctoken is ERC20 {
+    
+    address public owner;
+    uint private rate;
+    uint private supply;
+
+    
+    constructor() ERC20('mytoken','MYT'){
+       
+       _mint(msg.sender, 1000000 * 10 ** 18);
+       rate = 100;
+        owner = msg.sender;
+    }
+    
+     function buyToken () payable external {
+        _mint(msg.sender, msg.value * rate * 10 ** 18);
+     }
+     
+     fallback() external payable {
+        if (msg.value > 0 ) {
+            _mint(msg.sender, msg.value * rate * 10 ** 18 );
+        } 
+        
+    function changerate(address to , uint amount) external {
+        require(msg.sender == owner , "only owner can change the rate");
+        rate = amount;
+        
+    }     
+}
