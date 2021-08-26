3d7e3299cf5f480135f7c3d30c9c5ee2d85fc251
burhansaleemi <76588697+burhansaleemi@users.noreply.github.com>
2021-08-26 22:53:38 +0500

    Update assignment3b.sol

--- assignmentQ3_138761/assignment3b.sol
+++ assignmentQ3_138761/assignment3b.sol
@@ -1,24 +1,26 @@
 pragma solidity ^0.8.0;
 //"SPDX-License-Identifier: UNLICENSED"
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
-
-contract erctoken is ERC20 {
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol';
+contract erctoken is Ownable , ERC20 {
     
-    address public owner;
+    address public ownerof;
     uint private rate;
     uint private supply;
-    uint256 private tokenCap = 2000000 * 100 ** 18;
-    
+    uint private tokenCap = 2000000 * 100 ** 18;
+    uint private contractdeploytime;
+    uint public constant DELAY = 1 minutes;
     constructor() ERC20('mytoken','MYT'){
        
        _mint(msg.sender, 1000000 * 10 ** 18);
        rate = 100;
-        owner = msg.sender;
+        ownerof = msg.sender;
     }
     
      function buyToken () payable external {
         require(ERC20.totalSupply() + msg.value <= tokenCap, "ERC20Capped: Token Cap exceeded");
         _mint(msg.sender, msg.value * rate * 10 ** 18);
+        contractdeploytime =  block.timestamp;
      }
      
      fallback() external payable {
@@ -28,7 +30,7 @@ contract erctoken is ERC20 {
      }
      
       function changerate(uint amount) external {
-        require(msg.sender == owner , "only owner can change the rate");
+        require(msg.sender == ownerof , "only owner can change the rate");
         rate = amount;
         
     }     
@@ -36,4 +38,9 @@ contract erctoken is ERC20 {
      function cap() public view virtual returns (uint256) {
         return tokenCap;
      }
+     
+     function transfer_with_Delay(address recipient, uint256 amount) public virtual {
+        require(block.timestamp >= contractdeploytime + DELAY, "dealy time");
+        _transfer(_msgSender(), recipient, amount);
+    }
 }
